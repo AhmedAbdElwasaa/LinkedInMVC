24415fb2f85ad59525c90e592db8d3ea6c5793ab
Souhaila <sola.111me@gmail.com>
2019-04-02 13:05:00 +0200

    Add Post and Posts List

---
+++ LinkedInMVC/ViewModel/PostViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using LinkedInMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LinkedInMVC.ViewModel
+{
+    public class PostViewModel
+    {
+        public Post Post { get; set; }
+    }
+}
