d93bcfee46589421e27863dd61e917676e9a6161
Souhaila <sola.111me@gmail.com>
2019-04-03 12:36:21 +0200

    Design of comments added

--- LinkedInMVC/BLL/CommentManager.cs
+++ LinkedInMVC/BLL/CommentManager.cs
@@ -9,8 +9,26 @@ namespace LinkedInMVC.BLL
 {
     public class CommentManager : Repository<Comment, ApplicationDbContext>
     {
+        private ApplicationDbContext context;
         public CommentManager(ApplicationDbContext context) : base(context)
         {
+            this.context = context;
+        }
+        public List<Comment> GetCommentByPostId(int postId)
+        {
+            return context.Comments.Where(c => c.Fk_PostId == postId).ToList();
+        }
+
+        public List<Comment> GetCommentByPostIdAjax(int postId, int i)
+        {
+            var ajaxCmnts = new List<Comment>();
+            var allComments = GetCommentByPostId(postId);
+
+            for (; i < allComments.Count(); ++i)
+            {
+                ajaxCmnts.Add(allComments[i]);
+            }
+            return ajaxCmnts;
         }
     }
 }
