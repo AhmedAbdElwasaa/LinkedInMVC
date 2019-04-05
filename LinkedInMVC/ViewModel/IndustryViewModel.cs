43d52437b171c58ec58dc95790484892ed6ca600
Ahmed Abd-Elwasaa <ahmed_abdelwasaa@yahoo.com>
2019-04-05 21:03:45 +0200

    sdfkg

--- LinkedInMVC/ViewModel/IndustryViewModel.cs
+++ LinkedInMVC/ViewModel/IndustryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinkedInMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,5 +10,6 @@ namespace LinkedInMVC.ViewModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<Company> Companies { get; set; }
     }
 }
